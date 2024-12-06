017ab950ddfff7379a6ff097ca4473ca840de7af
kyw <kywkyw8379@gmai.com>
2024-12-06 12:37:21 +0900

    엔티티파일 김영우

---
+++ src/main/java/com/example/demo/entity/KywBoard.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class KywBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long num;  // pk
+
+    private String title;  // 글제목
+
+    private String content; // 글내용
+
+    private String writer;  // 작성자
+
+    private LocalDateTime localDateTime; // 작성날짜
+
+
+}
